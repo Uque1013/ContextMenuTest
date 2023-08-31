8071b88cab905b5ca0dfbf3b0af7ea87e27c895f
Uque1013 <kye6735@naver.com>
2023-08-31 19:13:26 +0900

    실습 7-2(Context menu) 완성

--- app/src/main/java/kr/hs/emirim/kye/contextmenutest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kye/contextmenutest/MainActivity.kt
@@ -1,7 +1,12 @@
 package kr.hs.emirim.kye.contextmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 
@@ -14,6 +19,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "컨텍스트 메뉴 연습"
+        linear = findViewById(R.id.linear)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
 
+        registerForContextMenu(btn1)
+        registerForContextMenu(btn2)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        var mInflater = this.menuInflater
+        if(v === btn1){
+            menu!!.setHeaderTitle("배경색 변경")
+            mInflater.inflate(R.menu.menu1, menu)
+        }
+
+        if(v === btn2){
+            mInflater.inflate(R.menu.menu2, menu)
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        super.onContextItemSelected(item)
+        when(item.itemId){
+            R.id.itemRed->{
+                linear.setBackgroundColor(Color.RED)
+                return true
+            }
+            R.id.itemBlue->{
+                linear.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            R.id.itemYellow->{
+                linear.setBackgroundColor(Color.YELLOW)
+                return true
+            }
+
+            R.id.subRotate->{
+                btn1.rotation = 180f
+                return true
+            }
+            R.id.subSize->{
+                btn1.scaleX = 2f
+                return true
+            }
+            R.id.subOriginal->{
+                btn1.rotation = 0f
+                btn1.scaleX = 1f
+                return true
+            }
+        }
+        return false
     }
 }
